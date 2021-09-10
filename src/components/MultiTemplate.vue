09d6a44031ea5f8e88cff10d17538229d03f63f5
azidyn <azidyn@protonmail.com>
2021-09-10 18:31:32 +0100

    add comps

---
+++ src/components/MultiTemplate.vue
@@ -0,0 +1,92 @@
+<template>
+  
+    <div>
+        <Title :text="`LOB Deltas (${Object.keys(this.connections.inputs).length})`"></Title>
+        {{ multi }}
+    </div>
+
+</template>
+
+
+<script>
+
+import Title from './common/Title.vue';
+import { Connection } from './mixins/Connection';
+
+const Settings = {
+    ports: {
+        input: ['Orderbook'],
+        output: 'TimeSeries'
+    },
+    connections: 1
+};
+
+export default {
+
+    mixins: [ Connection ],
+    props: ['config', 'size', 'id' ],
+
+    components: { Title },
+
+    data() {
+        return { 
+            multi: {} 
+        }
+    },
+
+    methods: {
+
+        update( data ) {
+
+        },
+
+
+        /* Set up our data */
+        input( opts ) {
+
+            const id = opts.sourceId;
+            this.$set( this.multi, id, { bid: 0, ask: 0 } );
+
+        },
+       
+        /* Clean up our data */
+        xinput( sourceId ) {
+            
+            delete this.multi[ sourceId ];
+
+        },
+
+        /* Is the connecting archetype compatible? */
+        verify( contract ) {
+
+            if ( !Settings.ports.input.includes( contract.output ) ) 
+                return { success: false, error: 'Input is not compatible' }
+
+            return { success: true }
+           
+        },
+
+
+        contract() {
+            return Settings.ports;
+        }
+
+    },
+
+    mounted() {
+
+        // $mitt.on( `${this.id}:snapshot`, this.update );
+
+    },
+
+
+    settings() {
+        return Settings;
+    }
+
+}
+</script>
+
+<style>
+
+</style>
